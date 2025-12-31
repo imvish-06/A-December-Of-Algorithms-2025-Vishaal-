7ec42bdaf04f0ae0f91b694fb0ed9910a67e1b6f
imvish-06 <gkvishaal@gmail.com>
2025-12-31 15:31:50 +0530

    Add student and sandwich counting logic
    Implement logic to count students and sandwiches, and calculate uneaten sandwiches.

---
+++ December - 08/Day8.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int students[n], sandwiches[n];
+    int count0 = 0, count1 = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &students[i]);
+        if (students[i] == 0)
+            count0++;
+        else
+            count1++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &sandwiches[i]);
+    }
+
+    int eaten = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (sandwiches[i] == 0) {
+            if (count0 == 0)
+                break;
+            count0--;
+        } else {
+            if (count1 == 0)
+                break;
+            count1--;
+        }
+        eaten++;
+    }
+
+    printf("%d", n - eaten);
+    return 0;
+}
